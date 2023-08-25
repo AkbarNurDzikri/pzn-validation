44a3a00e02f125462f7da7c9456ddedc55bdc6b2
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-08-25 15:31:28 +0700

    melanjutkan pemeriksaan error dengan menggunakan option men-disable abortEarly

--- tests/validation-error.test.js
+++ tests/validation-error.test.js
@@ -2,10 +2,10 @@ import Joi from 'joi';
 
 describe("Joi", () => {
   it("should return validation error", () => {
-    const usernameSchema = Joi.string().min(10).required();
-    const result = usernameSchema.validate("salahan");
+    const usernameSchema = Joi.string().min(10).email().required();
+    const result = usernameSchema.validate("454325", {abortEarly: false});
 
-    console.info(result);
+    // console.info(result.error.details);
     if(result.error) {
       result.error.details.forEach(detail => {
         console.info(`${detail.path} = ${detail.message}`);
