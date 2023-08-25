8ddbda951e267e6661aeab55b44f87f2fdc206e6
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-08-25 13:21:28 +0700

    date validation

--- tests/date.test.js
+++ tests/date.test.js
@@ -5,12 +5,16 @@ describe("Joi", () => {
     const birthDateSchema = Joi.date().required().max("now").min("8-17-1945");
     
     const resultFalse1 = birthDateSchema.validate("8-17-1944");
-    console.info(resultFalse1);
+    // console.info(resultFalse1);
+    console.info(typeof resultFalse1.value); // Date Object
+    console.info(typeof resultFalse1.error); // ValidationError Object
     
     const resultTrue = birthDateSchema.validate("8-3-1994");
-    console.info(resultTrue);
+    // console.info(resultTrue);
+    console.info(typeof resultTrue.value);
+    console.info(typeof resultTrue.error);
 
     const resultFalse2 = birthDateSchema.validate("8-17-2024");
-    console.info(resultFalse2);
+    // console.info(resultFalse2);
   });
 });
