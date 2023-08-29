1c44564a83145097aa24b8697f0410fb2209522f
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-08-29 15:04:37 +0700

    nested object validation

--- tests/validation-object.test.js
+++ tests/validation-object.test.js
@@ -1,22 +1,50 @@
 import Joi from 'joi';
 
 describe("Joi", () => {
-  it("should can validate object", () => {
-    const loginSchema = Joi.object({
-      username: Joi.string().min(3).email().required(),
-      password: Joi.string().min(6).max(100)
+  // it("should can validate object", () => {
+  //   const loginSchema = Joi.object({
+  //     username: Joi.string().min(3).email().required(),
+  //     password: Joi.string().min(6).max(100)
+  //   });
+
+  //   const request = {
+  //     username: "tes",
+  //     password: "123"
+  //   }
+
+  //   const results = loginSchema.validate(request, {abortEarly: false});
+  //   if(results.error) {
+  //     results.error.details.forEach(detail => {
+  //       console.info(`Error message: ${detail.message}`)
+  //     });
+  //   }
+  // });
+
+  it("should can validate nested object", () => {
+    const createUserSchema = Joi.object({
+      id: Joi.string().required().max(100),
+      name: Joi.string().required().max(100),
+      address: Joi.object({
+        street: Joi.string().required().max(200),
+        city: Joi.string().required().max(100),
+        country: Joi.string().required().max(100),
+        zipCode: Joi.string().required().max(10),
+      }).required()
     });
 
     const request = {
-      username: "tes",
-      password: "123"
-    }
+      address: {}
+    };
 
-    const results = loginSchema.validate(request, {abortEarly: false});
-    if(results.error) {
-      results.error.details.forEach(detail => {
-        console.info(`Error message: ${detail.message}`)
-      });
+    const result = createUserSchema.validate(request, {
+      abortEarly: false
+    });
+
+    console.info(result);
+    if(result.error) {
+      result.error.details.forEach(detail => {
+        console.info(`${detail.path}: ${detail.message}`)
+      })
     }
   });
 });
